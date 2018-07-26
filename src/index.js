da056da90a69dccc1f3a83e80944b94cdded8fb5
Fernando Souza <fernandostorm1@gmail.com>
2018-07-26 07:24:12 -0300

    remove key

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase'
-
-var config = {
-    apiKey: "AIzaSyAkbHcsMLWE92ps9qbgt4SUxiWZrh16GsA",
-    authDomain: "restaurant-menu-vianna.firebaseapp.com",
-    databaseURL: "https://restaurant-menu-vianna.firebaseio.com",
-    projectId: "restaurant-menu-vianna",
-    storageBucket: "restaurant-menu-vianna.appspot.com",
-    messagingSenderId: "1074219845719"
-};
+import config from './config-local'
 
 firebase.initializeApp(config);
 
