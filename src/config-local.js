55c236c1b3db3a1b96e0a26399101fdb628a351a
Fernando Souza <fernandovbsouza@gmail.com>
2018-08-18 14:22:27 -0300

    Ignore local config file

--- src/config-local.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyAkbHcsMLWE92ps9qbgt4SUxiWZrh16GsA",
-    authDomain: "restaurant-menu-vianna.firebaseapp.com",
-    databaseURL: "https://restaurant-menu-vianna.firebaseio.com",
-    projectId: "restaurant-menu-vianna",
-    storageBucket: "restaurant-menu-vianna.appspot.com",
-    messagingSenderId: "1074219845719"
-};
-export default config
